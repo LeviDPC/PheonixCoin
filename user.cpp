5e1e9e4e85d1d0389248c71938e2a1ee62d6acc5
Clefspear99 <43016747+Clefspear99@users.noreply.github.com>
2018-12-04 12:25:17 -0600

    Added a user class - isn't finished yet

---
+++ user.cpp
@@ -0,0 +1,80 @@
+#include "user.h"
+
+
+
+user::user(){}
+
+user::~user(){}
+
+user::user(string userName, string publickKey, long balance) {
+	user::userName = userName;
+	user::publicKey = publicKey;
+	user::balance = balance;
+
+}
+
+user::user(string publickKey, long balance) {
+	user::publicKey = publicKey;
+	user::balance = balance;
+}
+
+user::user(string publickKey) {
+	user::publicKey = publicKey;
+}
+
+string user::getUserName() const {
+	return user::userName;
+}
+
+string user::getPublicKey() const {
+	return user::publicKey;
+}
+
+long user::getBalance() const {
+	return user::balance;
+}
+
+void user::setUserName(string in) {
+	user::userName = in;
+}
+
+void user::setPublicKey(string in) {
+	user::publicKey = in;
+}
+
+void user::setBalance(long in) {
+	user::balance = in;
+}
+
+void user::addToBalance(long in) {
+	user::balance += in;
+}
+
+void user::subtractFromBalance(long in) {
+	user::balance -= in;
+}
+
+string user::toString() const{
+	stringstream stream;
+	stream << "Username: " << user::userName;
+	stream << " Public Key: " << user::publicKey;
+	stream << " Balance: " << user::balance;
+	return stream.str();
+}
+
+ostream &operator<<(ostream &stream, const user &in)
+{
+	stream << in.toString();
+	return stream;
+}
+
+
+user operator+=(const user &)
+{
+	return user();
+}
+
+user operator+=(const user &)
+{
+	return user();
+}
---
+++ user.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <string>
+#include <sstream>
+using namespace std;
+class user
+{
+
+private:
+	string userName="";
+	string publicKey="";
+	long balance=0;
+
+public:
+	user();
+	~user();
+	user(string userName, string publickKey, long balance);
+	user(string publickKey, long balance);
+	user(string publickKey);
+	
+	string getUserName() const;
+	string getPublicKey() const;
+	long getBalance() const;
+
+	void setUserName(string in);
+	void setPublicKey(string in);
+	void setBalance(long in);
+
+	void addToBalance(long in);
+	void subtractFromBalance(long in);
+
+	string toString() const;
+
+	friend ostream &operator << (ostream &, const user &);
+
+	bool operator > (const user &);
+	bool operator < (const user &);
+	
+
+};
+
